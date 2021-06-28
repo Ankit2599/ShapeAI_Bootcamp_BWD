fab4875e5a8d0c9ab56f40dbc1de869e3cfb8051
Ankit2599 <86562379+Ankit2599@users.noreply.github.com>
2021-06-28 12:25:21 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer(){
+  return (
+    
+      <footer>
+      <p>Copyright by Ankit Jain @ {new Date().getFullYear()}</p>
+      </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Info(){
+  return (
+    <div className="note">
+     <h1>Bitcoin </h1>
+     <p>Bitcoin (₿) is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries. Transactions are verified by network nodes through cryptography and recorded in a public distributed ledger called a blockchain. The cryptocurrency was invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto. The currency began use in 2009 when its implementation was released as open-source software</p>
+      </div>
+     
+  );
+}
+
+export default Info;
